80a89d9b54f4603055229cfc639f92bb8076ae34
Yuni CBM <yuni@u.northwestern.edu>
2020-10-15 13:09:55 -0600

    1015

--- assets/js/main.js
+++ assets/js/main.js
@@ -99,13 +99,13 @@
 			$window.on('load', function() {
 
 				$('#two').poptrox({
-					caption: function($a) { return $a.next('h3').text(); },
+					// caption: function($a) { return $a.next('h3').text(); },
 					overlayColor: '#2c2c2c',
 					overlayOpacity: 0.85,
 					popupCloserText: '',
 					popupLoaderText: '',
 					selector: '.work-item a.image',
-					usePopupCaption: true,
+					usePopupCaption: false,
 					usePopupDefaultStyling: false,
 					usePopupEasyClose: false,
 					usePopupNav: true,
